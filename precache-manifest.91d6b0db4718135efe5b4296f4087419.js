e2d15e343972061f7830cd56536cf5675958ef6f
wroblewskiwojciech161 <wroblewskiwojciech161@gmail.com>
2022-11-05 00:02:07 +0100

    initialized

---
+++ precache-manifest.91d6b0db4718135efe5b4296f4087419.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6800663330b21d00d1be",
+    "url": "/ThesisDefenceFront/css/app.644f55f3.css"
+  },
+  {
+    "revision": "2d71f802a12aaff837e8",
+    "url": "/ThesisDefenceFront/css/chunk-vendors.52a48aab.css"
+  },
+  {
+    "revision": "f21db61c93a7e72fc61748a1f5f69b1b",
+    "url": "/ThesisDefenceFront/index.html"
+  },
+  {
+    "revision": "6800663330b21d00d1be",
+    "url": "/ThesisDefenceFront/js/app.b02409d8.js"
+  },
+  {
+    "revision": "2d71f802a12aaff837e8",
+    "url": "/ThesisDefenceFront/js/chunk-vendors.32f85897.js"
+  },
+  {
+    "revision": "a49394d2befac14d6fb1ba0e909711ff",
+    "url": "/ThesisDefenceFront/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/ThesisDefenceFront/robots.txt"
+  }
+]);
